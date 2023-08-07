ba479557e884c7bd0839f01a798dcc0bcf8b8e8e
opoberezh <opoberezh.2015@gmail.com>
2023-08-07 21:40:19 +0200

    rerere

---
+++ src/components/QuizList.js
@@ -0,0 +1,14 @@
+import { QuizCard } from "./QuizCard";
+
+export const QuizList = ({items}) => {
+    return (
+        <ul>
+            {items.map(item => (
+                <li key={item.id}>
+                    <QuizCard item={item}/>
+                </li>
+            )
+                )}
+        </ul>
+    );
+};
