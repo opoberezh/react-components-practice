9c6fb48a9069470b2ac15317d0cb2b432336db82
opoberezh <opoberezh.2015@gmail.com>
2023-08-07 23:20:29 +0200

    ddd

---
+++ src/components/QuizCard/QuizCard.js
@@ -0,0 +1,13 @@
+import {Wrapper, Topic, Text} from './QuizCard.styled';
+
+export const QuizCard = ({item: {topic, level, time, questions}}) => {
+    return (
+        <Wrapper>
+            <Topic>{topic}</Topic>
+            <Text><b>Level:</b> {level}</Text>
+            <Text><b>Time:
+                </b> {time} min</Text>
+            <Text><b>Questions:</b>{questions}</Text>
+        </Wrapper>
+    );
+};
